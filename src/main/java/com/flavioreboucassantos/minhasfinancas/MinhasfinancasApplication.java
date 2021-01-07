cc98d233801e25565c05908e9e2094fd602cc14e
Flávio Rebouças Santos <FlavioReboucasSantos@gmail.com>
2021-01-07 16:02:57 -0300

    Adicionado CORS e Pequenas correções

--- src/main/java/com/flavioreboucassantos/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/flavioreboucassantos/minhasfinancas/MinhasfinancasApplication.java
@@ -2,9 +2,19 @@ package com.flavioreboucassantos.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc // resolve cors origin
+public class MinhasfinancasApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
--- src/main/java/com/flavioreboucassantos/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/flavioreboucassantos/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,12 +1,16 @@
 package com.flavioreboucassantos.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 	
 	private String nome;
