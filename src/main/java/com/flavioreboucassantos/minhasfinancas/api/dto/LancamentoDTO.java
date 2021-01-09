e786babac122d4511f440b61fad54df65016c558
Flávio Rebouças Santos <FlavioReboucasSantos@gmail.com>
2021-01-09 20:32:17 -0300

    Pequena correção

--- src/main/java/com/flavioreboucassantos/minhasfinancas/api/dto/LancamentoDTO.java
+++ src/main/java/com/flavioreboucassantos/minhasfinancas/api/dto/LancamentoDTO.java
@@ -2,11 +2,15 @@ package com.flavioreboucassantos.minhasfinancas.api.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
 	private Long id;
 	private String descricao;
